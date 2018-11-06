e75532162e4d4d7df44c7cbf4c945c32cfe6ab0c
RafaelLimaReis <rafaellimareis01@gmail.com>
2018-11-06 15:54:58 -0200

    closes #1 Create method of set configs in connection in the file .env

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
+require('dotenv').config();
+require('./helpers/database').connect();
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-
-mongoose.connect('mongodb://goweek:goweek123@ds133856.mlab.com:33856/goweek_backend', {
-  useNewUrlParser: true
-});
 
 app.use((req, res, next) => {
   req.io = io;
